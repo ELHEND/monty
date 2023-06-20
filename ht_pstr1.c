e41a570f10b57a38b0a2ffaf09a7ae8b5f5e1fd2
ELHEND <alhnd683@gmail.com>
2023-06-20 10:34:52 -0700

    uy

--- ht_pstr1.c
+++ ht_pstr1.c
@@ -12,18 +12,19 @@ void f_pstr(stack_t **head, unsigned int counter)
 
 {
 
-		stack_t *H;
-			(void)counter;
+	stack_t *H;
+	(void)counter;
 
-				H = *head;
-					while (H)
-							{
-										if (H->n > 127 || H->n <= 0)
-													{
-																	break;
-																			}
-												printf("%c", H->n);
-														H = H->next;
-															}
-						printf("\n");
+	H = *head;
+	while (H)
+	{
+		if (H->n > 127 || H->n <= 0)
+		{
+			break;
+		}
+		printf("%c", H->n);
+		H = H->next;
+	}
+	printf("\n");
+
 }
