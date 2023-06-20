ab5cc8ebbaa998d43a06be35c11aaf1b7bab7140
ELHEND <alhnd683@gmail.com>
2023-06-20 09:47:52 -0700

    f

--- ht_sub1.c
+++ ht_sub1.c
@@ -11,7 +11,9 @@ void f_sub(stack_t **head, unsigned int counter)
 {
 	stack_t *aux;
 	int sus, nodes;
+
 	aux = *head;
+
 	for (nodes = 0; aux != NULL; nodes++)
 		aux = aux->next;
 	if (nodes < 2)
