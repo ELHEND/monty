af019f252a25f4fc18913d8ec67f3f88ee528499
ELHEND <alhnd683@gmail.com>
2023-06-22 12:10:11 -0700

    gbhk

--- main.c
+++ main.c
@@ -1,47 +1,44 @@
-#define _GNU_SOURCE
-
 #include "monty.h"
-
 /**
- *  * main - monty code interpreter
- *   * @argc: number of arguments
- *    * @argv: array of arguments
- *     *
- *      * Return: 0 on success, otherwise EXIT_FAILURE
+ *  * * main - monty code interpreter
+ *   * * @argv: monty file location
+ *    * * @argc: number of arguments
+ *     * * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
+	bus_t bus = {NULL, NULL, NULL, 0};
+	char *content;
 	FILE *file;
-	char *line = NULL;
-	size_t line_size = 0;
-	ssize_t read_line = 0;
-	unsigned int line_no = 0;
+	size_t size = 0;
+	ssize_t read_line = 1;
 	stack_t *stack = NULL;
-
+	unsigned int counter = 0;
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
-		return (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
-
 	file = fopen(argv[1], "r");
+	bus.file = file;
 	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		return (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
-
-	while ((read_line = getline(&line, &line_size, file)) != -1)
+	while (read_line > 0)
 	{
-		line_no++;
-		execute(line, &stack, line_no, file);
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
 	}
-
 	free_stack(stack);
 	fclose(file);
-	if (line)
-		free(line);
-
-	return (EXIT_SUCCESS);
+	return (0);
 }
-
