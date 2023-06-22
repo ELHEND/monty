edb74dbba0fabd1423bae38c643bcac49ba5b8a0
ELHEND <alhnd683@gmail.com>
2023-06-22 12:02:23 -0700

    smnv

--- main.c
+++ main.c
@@ -1,49 +1,47 @@
 #include "monty.h"
-
-bus_t bus = {NULL, NULL, NULL, 0};
-
 /**
  *  * main - monty code interpreter
  *   * @argc: number of arguments
  *    * @argv: array of arguments
  *     *
  *      * Return: 0 on success, otherwise EXIT_FAILURE
- *       */
+ */
 int main(int argc, char *argv[])
 {
-	    FILE *file;
-	        char *line = NULL;
-		    size_t line_size = 0;
-		        ssize_t read_line = 0;
-			    unsigned int line_no = 0;
-			        stack_t *stack = NULL;
+	bus_t bus = {NULL, NULL, NULL, 0};
+	FILE *file;
+	char *line = NULL;
+	size_t line_size = 0;
+	ssize_t read_line = 0;
+	unsigned int line_no = 0;
+	stack_t *stack = NULL;
 
-				    if (argc != 2)
-					        {
-							        fprintf(stderr, "USAGE: monty file\n");
-								        return (EXIT_FAILURE);
-									    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		return (EXIT_FAILURE);
+	}
 
-				        file = fopen(argv[1], "r");
-					    if (!file)
-						        {
-								        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-									        return (EXIT_FAILURE);
-										    }
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		return (EXIT_FAILURE);
+	}
 
-					        bus.file = file;
-						    while ((read_line = getline(&line, &line_size, file)) != -1)
-							        {
-									        line_no++;
-										        bus.content = line;
-											        execute(line, &stack, line_no, file);
-												    }
+	bus.file = file;
+	while ((read_line = getline(&line, &line_size, file)) != -1)
+	{
+		line_no++;
+		bus.content = line;
+		execute(line, &stack, line_no, file);
+	}
 
-						        free_stack(stack);
-							    fclose(file);
-							        if (line)
-									        free(line);
+	free_stack(stack);
+	fclose(file);
+	if (line)
+		free(line);
 
-								    return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
 
