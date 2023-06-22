eec9e86ac75db1da2633ceb0d6a7da0f04298e5f
ELHEND <alhnd683@gmail.com>
2023-06-22 11:58:44 -0700

    fgbv

--- main.c
+++ main.c
@@ -3,45 +3,47 @@
 bus_t bus = {NULL, NULL, NULL, 0};
 
 /**
- * * main - monty code interpreter
- * * @argv: monty file location
- * * @argc: number of arguments
- * * Return: 0 on success
- */
+ *  * main - monty code interpreter
+ *   * @argc: number of arguments
+ *    * @argv: array of arguments
+ *     *
+ *      * Return: 0 on success, otherwise EXIT_FAILURE
+ *       */
 int main(int argc, char *argv[])
 {
-	char *content;
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
+	    FILE *file;
+	        char *line = NULL;
+		    size_t line_size = 0;
+		        ssize_t read_line = 0;
+			    unsigned int line_no = 0;
+			        stack_t *stack = NULL;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (read_line > 0)
-	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
-		{
-			execute(content, &stack, counter, file);
-		}
-		free(content);
-	}
-	free_stack(stack);
-	fclose(file);
-	return (0);
+				    if (argc != 2)
+					        {
+							        fprintf(stderr, "USAGE: monty file\n");
+								        return (EXIT_FAILURE);
+									    }
+
+				        file = fopen(argv[1], "r");
+					    if (!file)
+						        {
+								        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+									        return (EXIT_FAILURE);
+										    }
+
+					        bus.file = file;
+						    while ((read_line = getline(&line, &line_size, file)) != -1)
+							        {
+									        line_no++;
+										        bus.content = line;
+											        execute(line, &stack, line_no, file);
+												    }
+
+						        free_stack(stack);
+							    fclose(file);
+							        if (line)
+									        free(line);
+
+								    return (EXIT_SUCCESS);
 }
+
